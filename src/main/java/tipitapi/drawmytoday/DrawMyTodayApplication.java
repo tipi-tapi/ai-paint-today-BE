99c55f53a8904c3d3241b8f31b5b9c27c65a0c2f
akalswl14 <gnkryd12@gmail.com>
2023-04-21 16:59:50 +0900

    :sparkles: feature: 프로젝트 세팅 및 도커파일 추가

---
+++ src/main/java/tipitapi/drawmytoday/DrawMyTodayApplication.java
@@ -0,0 +1,13 @@
+package tipitapi.drawmytoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawMyTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawMyTodayApplication.class, args);
+	}
+
+}
