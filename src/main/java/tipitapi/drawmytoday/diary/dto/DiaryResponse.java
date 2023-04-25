7097f06eb42ed257f579e67566cdad7433def6fd
akalswl14 <gnkryd12@gmail.com>
2023-04-25 20:03:11 +0900

    :sparkles: feature: DiaryResponse DTO 생성 메소드 추가

--- src/main/java/tipitapi/drawmytoday/diary/dto/DiaryResponse.java
+++ src/main/java/tipitapi/drawmytoday/diary/dto/DiaryResponse.java
@@ -3,13 +3,18 @@ package tipitapi.drawmytoday.diary.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import tipitapi.drawmytoday.diary.domain.Diary;
+import tipitapi.drawmytoday.diary.domain.EmotionRecord;
+import tipitapi.drawmytoday.diary.domain.Image;
 
 @Getter
 @Schema(description = "일기 상세 Response")
 @AllArgsConstructor
 public class DiaryResponse {
+
   @Schema(description = "일기 아이디")
   private final Long id;
 
@@ -22,9 +27,16 @@ public class DiaryResponse {
   @Schema(description = "일기 작성 시간")
   private final LocalDateTime createdAt;
 
+  @Schema(description = "감정 목록")
+  private final List<String> emotions;
+
   @Schema(description = "일기 내용")
   private String notes;
 
-  @Schema(description = "감정 목록")
-  private final List<String> emotions;
+  public static DiaryResponse of(Diary diary, Image image, List<EmotionRecord> records) {
+    List<String> emotions = records.stream().map((eachRecord) -> eachRecord.getEmotion().getName())
+        .collect(Collectors.toList());
+    return new DiaryResponse(diary.getDiaryId(), image.getImageUrl(), diary.getDiaryDate(),
+        diary.getCreatedAt(), emotions, diary.getNotes());
+  }
 }
