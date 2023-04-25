353dda9a0a2232abeee6eb8c555ff23bf8f79c17
akalswl14 <gnkryd12@gmail.com>
2023-04-26 01:23:19 +0900

    :sparkles: feature: CustomException -> BusinessException 이름 변경

--- src/main/java/tipitapi/drawmytoday/user/exception/UserNotFoundException.java
+++ src/main/java/tipitapi/drawmytoday/user/exception/UserNotFoundException.java
@@ -1,9 +1,9 @@
 package tipitapi.drawmytoday.user.exception;
 
-import tipitapi.drawmytoday.common.exception.CustomException;
+import tipitapi.drawmytoday.common.exception.BusinessException;
 import tipitapi.drawmytoday.common.exception.ErrorCode;
 
-public class UserNotFoundException extends CustomException {
+public class UserNotFoundException extends BusinessException {
 
   public UserNotFoundException() {
     super(ErrorCode.USER_NOT_FOUND);
