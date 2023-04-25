2794cad7615c4578bc689b538da1c4daf2107e14
akalswl14 <gnkryd12@gmail.com>
2023-04-25 17:57:26 +0900

    :sparkles: feature: BaseEntity 추가

---
+++ src/main/java/tipitapi/drawmytoday/common/entity/BaseEntityWithUpdate.java
@@ -0,0 +1,22 @@
+package tipitapi.drawmytoday.common.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntityWithUpdate extends BaseEntity{
+
+  @NotNull
+  @LastModifiedDate
+  @Column(nullable = false)
+  private LocalDateTime updatedAt;
+}
