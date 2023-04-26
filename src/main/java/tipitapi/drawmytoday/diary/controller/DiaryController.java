def75f8059be01c7bbace5aa261c8e2dd8064ed2
akalswl14 <gnkryd12@gmail.com>
2023-04-26 13:30:47 +0900

    :recycle: refactor: @RestController 로 어노테이션 변경

--- src/main/java/tipitapi/drawmytoday/diary/controller/DiaryController.java
+++ src/main/java/tipitapi/drawmytoday/diary/controller/DiaryController.java
@@ -10,15 +10,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import tipitapi.drawmytoday.common.response.SuccessResponse;
 import tipitapi.drawmytoday.diary.dto.DiaryResponse;
 import tipitapi.drawmytoday.diary.service.DiaryService;
 
-@Controller
+@RestController
 @RequestMapping("/diary")
 @RequiredArgsConstructor
 public class DiaryController {
