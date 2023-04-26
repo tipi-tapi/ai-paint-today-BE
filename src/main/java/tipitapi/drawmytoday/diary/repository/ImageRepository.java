dc26f405df1ce59f653ad578468429522493d72b
akalswl14 <gnkryd12@gmail.com>
2023-04-26 13:28:05 +0900

    :recycle: refactor: @Repository 어노테이션 제거

--- src/main/java/tipitapi/drawmytoday/diary/repository/ImageRepository.java
+++ src/main/java/tipitapi/drawmytoday/diary/repository/ImageRepository.java
@@ -2,11 +2,9 @@ package tipitapi.drawmytoday.diary.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import tipitapi.drawmytoday.diary.domain.Diary;
 import tipitapi.drawmytoday.diary.domain.Image;
 
-@Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
   Optional<Image> findByIsSelectedTrueAndDiary(Diary diary);
