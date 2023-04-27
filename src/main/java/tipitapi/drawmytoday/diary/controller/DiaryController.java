803ea532345ef131e569f7595ccc1f3a4ede922d
choihuk <love72huk@naver.com>
2023-04-27 16:32:03 +0900

    :recycle: refactor: DiaryController getDiary 메서드 TODO 구현

--- src/main/java/tipitapi/drawmytoday/diary/controller/DiaryController.java
+++ src/main/java/tipitapi/drawmytoday/diary/controller/DiaryController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tipitapi.drawmytoday.common.resolver.AuthUser;
 import tipitapi.drawmytoday.common.response.SuccessResponse;
+import tipitapi.drawmytoday.common.security.jwt.JwtTokenInfo;
 import tipitapi.drawmytoday.diary.dto.DiaryResponse;
 import tipitapi.drawmytoday.diary.service.DiaryService;
 
@@ -23,29 +25,29 @@ import tipitapi.drawmytoday.diary.service.DiaryService;
 @RequiredArgsConstructor
 public class DiaryController {
 
-  private final DiaryService diaryService;
+    private final DiaryService diaryService;
 
-  @Operation(summary = "일기 조회", description = "특정 일기의 내용을 반환한다.")
-  @ApiResponses(value = {
-      @ApiResponse(
-          responseCode = "200",
-          description = "일기 상세 정보"),
-      @ApiResponse(
-          responseCode = "403",
-          description = "D002 : 자신의 일기에만 접근할 수 있습니다.",
-          content = @Content(schema = @Schema(hidden = true))),
-      @ApiResponse(
-          responseCode = "404",
-          description = "D001 : 일기를 찾을 수 없습니다.\t\nI001 : 선택된 이미지를 찾을 수 없습니다.",
-          content = @Content(schema = @Schema(hidden = true))),
-  })
-  @GetMapping("/{id}")
-  public ResponseEntity<SuccessResponse<DiaryResponse>> getDiary(
-      @Parameter(description = "가게 id", in = ParameterIn.PATH) @PathVariable("id") Long diaryId) {
-    // TODO : Authentication에서 가져오도록, Spring Security 추가 후 수정 필요.
-    Long userId = 1L;
-    return SuccessResponse.of(
-        diaryService.getDiary(userId, diaryId)
-    ).asHttp(HttpStatus.OK);
-  }
+    @Operation(summary = "일기 조회", description = "특정 일기의 내용을 반환한다.")
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "일기 상세 정보"),
+        @ApiResponse(
+            responseCode = "403",
+            description = "D002 : 자신의 일기에만 접근할 수 있습니다.",
+            content = @Content(schema = @Schema(hidden = true))),
+        @ApiResponse(
+            responseCode = "404",
+            description = "D001 : 일기를 찾을 수 없습니다.\t\nI001 : 선택된 이미지를 찾을 수 없습니다.",
+            content = @Content(schema = @Schema(hidden = true))),
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<SuccessResponse<DiaryResponse>> getDiary(
+        @Parameter(description = "가게 id", in = ParameterIn.PATH) @PathVariable("id") Long diaryId,
+        @AuthUser JwtTokenInfo tokenInfo) {
+        Long userId = tokenInfo.getUserId();
+        return SuccessResponse.of(
+            diaryService.getDiary(userId, diaryId)
+        ).asHttp(HttpStatus.OK);
+    }
 }
