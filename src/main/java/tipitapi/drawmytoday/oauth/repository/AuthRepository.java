3d26073e4611dca826283b0efe3115f4563f072a
choihuk <love72huk@naver.com>
2023-05-10 19:34:19 +0900

    :hammer: refactor: AutoRepository auth 패키지로 이동

--- src/main/java/tipitapi/drawmytoday/oauth/repository/AuthRepository.java
+++ src/main/java/tipitapi/drawmytoday/oauth/repository/AuthRepository.java
@@ -1,8 +1,8 @@
-package tipitapi.drawmytoday.user.repository;
+package tipitapi.drawmytoday.oauth.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import tipitapi.drawmytoday.user.domain.Auth;
+import tipitapi.drawmytoday.oauth.domain.Auth;
 import tipitapi.drawmytoday.user.domain.User;
 
 public interface AuthRepository extends JpaRepository<Auth, Long> {
