1b361e401b82e8b706c490be771ce217a5c64f56
akalswl14 <gnkryd12@gmail.com>
2023-05-16 20:34:47 +0900

    :sparkles: feature: LocalDateTime을 역직렬화하기한 처리 추가

--- src/main/java/tipitapi/drawmytoday/diary/dto/GetDiaryResponse.java
+++ src/main/java/tipitapi/drawmytoday/diary/dto/GetDiaryResponse.java
@@ -1,5 +1,10 @@
 package tipitapi.drawmytoday.diary.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import java.time.LocalDateTime;
@@ -20,9 +25,15 @@ public class GetDiaryResponse {
     @Schema(description = "이미지 URL", requiredMode = RequiredMode.REQUIRED)
     private final String imageUrl;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @Schema(description = "일기 날짜", requiredMode = RequiredMode.REQUIRED)
     private final LocalDateTime date;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @Schema(description = "일기 작성 시간", requiredMode = RequiredMode.REQUIRED)
     private final LocalDateTime createdAt;
 
