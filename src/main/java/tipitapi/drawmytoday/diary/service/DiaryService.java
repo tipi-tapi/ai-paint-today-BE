d6d3a0de694c550ce913f0e08f21fc5855b72242
akalswl14 <gnkryd12@gmail.com>
2023-05-16 20:30:11 +0900

    :sparkles: feature: getDiary 메소드가 User를 파라미터로 받도록 수정

--- src/main/java/tipitapi/drawmytoday/diary/service/DiaryService.java
+++ src/main/java/tipitapi/drawmytoday/diary/service/DiaryService.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tipitapi.drawmytoday.diary.domain.Diary;
 import tipitapi.drawmytoday.diary.domain.Image;
-import tipitapi.drawmytoday.diary.dto.DiaryResponse;
+import tipitapi.drawmytoday.diary.dto.GetDiaryResponse;
 import tipitapi.drawmytoday.diary.exception.DiaryNotFoundException;
 import tipitapi.drawmytoday.diary.exception.NotOwnerOfDiaryException;
 import tipitapi.drawmytoday.diary.repository.DiaryRepository;
+import tipitapi.drawmytoday.user.domain.User;
 
 @Service
 @Transactional(readOnly = true)
@@ -19,12 +20,12 @@ public class DiaryService {
     private final DiaryRepository diaryRepository;
     private final ImageService imageService;
 
-    public DiaryResponse getDiary(Long userId, Long diaryId) {
         Diary diary = diaryRepository.findById(diaryId).orElseThrow(DiaryNotFoundException::new);
         ownedByUser(userId, diary);
         Image image = imageService.getImage(diary);
         return DiaryResponse.of(diary, image, diary.getEmotion());
     }
+    public GetDiaryResponse getDiary(User user, Long diaryId) {
 
     private void ownedByUser(Long userId, Diary diary) {
         if (!Objects.equals(diary.getUser().getUserId(), userId)) {
