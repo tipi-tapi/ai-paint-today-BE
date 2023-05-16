73ac4da761603553a9113b44214d698bb1585023
akalswl14 <gnkryd12@gmail.com>
2023-05-16 20:33:47 +0900

    :sparkles: feature: 일기를 ID와 User로 조회하는 메소드 추가

--- src/main/java/tipitapi/drawmytoday/diary/repository/DiaryRepository.java
+++ src/main/java/tipitapi/drawmytoday/diary/repository/DiaryRepository.java
@@ -1,8 +1,11 @@
 package tipitapi.drawmytoday.diary.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tipitapi.drawmytoday.diary.domain.Diary;
+import tipitapi.drawmytoday.user.domain.User;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
+    Optional<Diary> findByIdAndUser(Long id, User user);
 }
