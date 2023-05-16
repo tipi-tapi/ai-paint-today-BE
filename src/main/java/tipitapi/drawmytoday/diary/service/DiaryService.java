8d272940226717bad9f3b61998257c1d9a1e38d6
akalswl14 <gnkryd12@gmail.com>
2023-05-16 20:32:20 +0900

    :sparkles: feature: 특정 일기 조회 메소드 getDiary 개선
    원래 API의 목적에 맞게 수정함.

--- src/main/java/tipitapi/drawmytoday/diary/service/DiaryService.java
+++ src/main/java/tipitapi/drawmytoday/diary/service/DiaryService.java
@@ -1,6 +1,5 @@
 package tipitapi.drawmytoday.diary.service;
 
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,7 +7,6 @@ import tipitapi.drawmytoday.diary.domain.Diary;
 import tipitapi.drawmytoday.diary.domain.Image;
 import tipitapi.drawmytoday.diary.dto.GetDiaryResponse;
 import tipitapi.drawmytoday.diary.exception.DiaryNotFoundException;
-import tipitapi.drawmytoday.diary.exception.NotOwnerOfDiaryException;
 import tipitapi.drawmytoday.diary.repository.DiaryRepository;
 import tipitapi.drawmytoday.user.domain.User;
 
@@ -20,12 +18,11 @@ public class DiaryService {
     private final DiaryRepository diaryRepository;
     private final ImageService imageService;
 
-        Diary diary = diaryRepository.findById(diaryId).orElseThrow(DiaryNotFoundException::new);
-        ownedByUser(userId, diary);
-        Image image = imageService.getImage(diary);
-        return DiaryResponse.of(diary, image, diary.getEmotion());
-    }
     public GetDiaryResponse getDiary(User user, Long diaryId) {
+        Diary diary = diaryRepository.findByIdAndUser(diaryId, user)
+            .orElseThrow(DiaryNotFoundException::new);
 
+        Image image = imageService.getImage(diary);
+        return GetDiaryResponse.of(diary, image, diary.getEmotion());
     }
 }
