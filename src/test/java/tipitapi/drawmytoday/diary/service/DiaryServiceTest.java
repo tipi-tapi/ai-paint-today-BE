6b61d4774e1efffd452afb2fe059e11e226b6f94
akalswl14 <gnkryd12@gmail.com>
2023-05-17 21:14:07 +0900

    :white_check_mark: test: DiaryServiceTest 개선

--- src/test/java/tipitapi/drawmytoday/diary/service/DiaryServiceTest.java
+++ src/test/java/tipitapi/drawmytoday/diary/service/DiaryServiceTest.java
@@ -3,8 +3,12 @@ package tipitapi.drawmytoday.diary.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
+import static tipitapi.drawmytoday.common.testdata.TestDiary.createDiaryWithId;
+import static tipitapi.drawmytoday.common.testdata.TestEmotion.createEmotion;
+import static tipitapi.drawmytoday.common.testdata.TestImage.createImage;
+import static tipitapi.drawmytoday.common.testdata.TestUser.createUser;
+import static tipitapi.drawmytoday.common.testdata.TestUser.createUserWithId;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -13,16 +17,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 import tipitapi.drawmytoday.diary.domain.Diary;
 import tipitapi.drawmytoday.diary.domain.Image;
 import tipitapi.drawmytoday.diary.dto.GetDiaryResponse;
 import tipitapi.drawmytoday.diary.exception.DiaryNotFoundException;
-import tipitapi.drawmytoday.diary.exception.ImageNotFoundException;
+import tipitapi.drawmytoday.diary.exception.NotOwnerOfDiaryException;
 import tipitapi.drawmytoday.diary.repository.DiaryRepository;
-import tipitapi.drawmytoday.emotion.domain.Emotion;
-import tipitapi.drawmytoday.user.domain.SocialCode;
 import tipitapi.drawmytoday.user.domain.User;
+import tipitapi.drawmytoday.user.service.ValidateUserService;
 
 @ExtendWith(MockitoExtension.class)
 class DiaryServiceTest {
@@ -32,26 +34,9 @@ class DiaryServiceTest {
     @Mock
     ImageService imageService;
     @Mock
-    Image image;
+    ValidateUserService validateUserService;
     @InjectMocks
     DiaryService diaryService;
-
-    private User createUser() {
-        return User.create(SocialCode.GOOGLE);
-    }
-
-    private Diary createDiary(Long diaryId, User user) {
-        Emotion emotion = Emotion.create("HAPPY", "#12312", true, "example emotion prompt",
-            "example color prompt");
-        Diary diary = Diary.builder().user(user).emotion(emotion)
-            .diaryDate(LocalDateTime.now()).isAi(true).build();
-        ReflectionTestUtils.setField(diary, "diaryId", diaryId);
-        return diary;
-    }
-
-    private Image createImage(Diary diary) {
-        return Image.create(diary, "https://example.com/image.jpg", true);
-    }
 
     @Nested
     @DisplayName("getDiary 메소드 테스트")
@@ -64,52 +49,51 @@ class DiaryServiceTest {
             @Test
             @DisplayName("일기를 반환한다.")
             void it_returns_diary() {
-                User user = createUser();
-                Diary diary = createDiary(1L, user);
+                User user = createUserWithId(1L);
+                Diary diary = createDiaryWithId(1L, user, createEmotion());
                 Image image = createImage(diary);
 
-                given(diaryRepository.findByDiaryIdAndUser(1L, user)).willReturn(
+                given(validateUserService.validateUserById(1L)).willReturn(user);
+                given(diaryRepository.findById(1L)).willReturn(
                     Optional.of(diary));
                 given(imageService.getImage(diary)).willReturn(image);
 
-                GetDiaryResponse getDiaryResponse = diaryService.getDiary(user, 1L);
+                GetDiaryResponse getDiaryResponse = diaryService.getDiary(1L, 1L);
 
                 assertThat(getDiaryResponse.getId()).isEqualTo(diary.getDiaryId());
             }
         }
 
         @Nested
-        @DisplayName("주어진 일기가 없거나, 유저의 일기가 아닌 경우")
+        @DisplayName("주어진 일기가 없을 경우")
         class if_diary_not_exists_or_not_user_owned_diary {
 
             @Test
-            @DisplayName("예외를 발생시킨다.")
-            void it_throws_exception() {
-                User user = createUser();
+            @DisplayName("DiaryNotFoundException 예외를 발생시킨다.")
+            void it_throws_DiaryNotFoundException() {
+                given(diaryRepository.findById(1L)).willReturn(Optional.empty());
+                given(validateUserService.validateUserById(1L)).willReturn(createUser());
 
-                given(diaryRepository.findByDiaryIdAndUser(1L, user)).willReturn(Optional.empty());
-
-                assertThatThrownBy(() -> diaryService.getDiary(user, 1L))
+                assertThatThrownBy(() -> diaryService.getDiary(1L, 1L))
                     .isInstanceOf(DiaryNotFoundException.class);
             }
         }
 
         @Nested
-        @DisplayName("일기에 이미지가 존재하지 않는 경우")
-        class if_diary_has_no_image {
+        @DisplayName("주어진 유저 소유의 일기가 아닐 경우")
+        class if_diary_not_user_owned {
 
             @Test
-            @DisplayName("예외를 발생시킨다.")
-            void it_throws_exception() {
-                User user = createUser();
-                Diary diary = createDiary(1L, user);
+            @DisplayName("NotOwnerOfDiaryException 예외를 발생시킨다.")
+            void it_throws_NotOwnerOfDiaryException() {
+                createUserWithId(1L);
+                User otherUser = createUserWithId(2L);
+                Diary diary = createDiaryWithId(1L, otherUser, createEmotion());
 
-                given(diaryRepository.findByDiaryIdAndUser(1L, user)).willReturn(
-                    Optional.of(diary));
-                given(imageService.getImage(diary)).willThrow(ImageNotFoundException.class);
+                given(diaryRepository.findById(1L)).willReturn(Optional.of(diary));
 
-                assertThatThrownBy(() -> diaryService.getDiary(user, 1L))
-                    .isInstanceOf(ImageNotFoundException.class);
+                assertThatThrownBy(() -> diaryService.getDiary(1L, 1L))
+                    .isInstanceOf(NotOwnerOfDiaryException.class);
             }
         }
     }
