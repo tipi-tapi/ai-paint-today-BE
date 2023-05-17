8fce72785be09773abd2e34d20a699b8ae6eb53e
akalswl14 <gnkryd12@gmail.com>
2023-05-17 21:14:33 +0900

    :white_check_mark: test: DiaryRepositoryTest 개선

--- src/test/java/tipitapi/drawmytoday/diary/repository/DiaryRepositoryTest.java
+++ src/test/java/tipitapi/drawmytoday/diary/repository/DiaryRepositoryTest.java
@@ -2,7 +2,6 @@ package tipitapi.drawmytoday.diary.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -11,50 +10,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.util.ReflectionTestUtils;
+import tipitapi.drawmytoday.common.BaseRepositoryTest;
 import tipitapi.drawmytoday.diary.domain.Diary;
-import tipitapi.drawmytoday.emotion.domain.Emotion;
-import tipitapi.drawmytoday.emotion.repository.EmotionRepository;
-import tipitapi.drawmytoday.user.domain.SocialCode;
 import tipitapi.drawmytoday.user.domain.User;
-import tipitapi.drawmytoday.user.repository.UserRepository;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
-class DiaryRepositoryTest {
+class DiaryRepositoryTest extends BaseRepositoryTest {
 
     @Autowired
     DiaryRepository diaryRepository;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    EmotionRepository emotionRepository;
-
-    private User createUser() {
-        User user = User.create(SocialCode.GOOGLE);
-        userRepository.save(user);
-        return user;
-    }
-
-    private Diary createDiary(Long diaryId, User user) {
-        Emotion emotion = Emotion.create("HAPPY", "#12312", true, "example emotion prompt",
-            "example color prompt");
-        emotionRepository.save(emotion);
-
-        Diary diary = Diary.builder().user(user).emotion(emotion)
-            .diaryDate(LocalDateTime.now()).isAi(true).build();
-        ReflectionTestUtils.setField(diary, "diaryId", diaryId);
-        diaryRepository.save(diary);
-
-        return diary;
-    }
 
     @Nested
-    @DisplayName("findByDiaryIdAndUser 메소드 테스트")
+    @DisplayName("findByDiaryId 메소드 테스트")
     class findByDiaryIdAndUserTest {
 
         @Nested
-        @DisplayName("유저 소유의 주어진 일기가 존재할 경우")
+        @DisplayName("주어진 일기가 존재할 경우")
         class if_diary_of_user_exists {
 
             @Test
@@ -63,7 +35,7 @@ class DiaryRepositoryTest {
                 User user = createUser();
                 Diary diary = createDiary(1L, user);
 
-                Optional<Diary> foundDiary = diaryRepository.findByDiaryIdAndUser(1L, user);
+                Optional<Diary> foundDiary = diaryRepository.findById(1L);
 
                 assertThat(foundDiary.isPresent()).isTrue();
                 assertThat(foundDiary.get().getDiaryId()).isEqualTo(diary.getDiaryId());
@@ -77,27 +49,7 @@ class DiaryRepositoryTest {
             @Test
             @DisplayName("null을 반환한다.")
             void return_null() {
-                User user = createUser();
-
-                Optional<Diary> foundDiary = diaryRepository.findByDiaryIdAndUser(1L, user);
-
-                assertThat(foundDiary).isEmpty();
-            }
-        }
-
-        @Nested
-        @DisplayName("주어진 user의 일기가 아니면")
-        class if_diary_not_owned_user {
-
-            @Test
-            @DisplayName("null을 반환한다.")
-            void return_null() {
-                User user = createUser();
-                Diary diary = createDiary(1L, user);
-                User otherUser = createUser();
-
-                Optional<Diary> foundDiary = diaryRepository.findByDiaryIdAndUser(1L,
-                    otherUser);
+                Optional<Diary> foundDiary = diaryRepository.findById(1L);
 
                 assertThat(foundDiary).isEmpty();
             }
