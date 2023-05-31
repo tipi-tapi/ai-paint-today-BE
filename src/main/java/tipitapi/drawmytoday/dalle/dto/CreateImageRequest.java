3c81d743749ddfd2c109bca925411cba0290ebd0
akalswl14 <gnkryd12@gmail.com>
2023-05-31 14:27:44 +0900

    :sparkles: feature: RestTemplate 요청시 필요한 기본 생성자 추가

--- src/main/java/tipitapi/drawmytoday/dalle/dto/CreateImageRequest.java
+++ src/main/java/tipitapi/drawmytoday/dalle/dto/CreateImageRequest.java
@@ -2,7 +2,11 @@ package tipitapi.drawmytoday.dalle.dto;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreateImageRequest {
 
