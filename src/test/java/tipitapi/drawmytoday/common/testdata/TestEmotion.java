e868f9cd1d6879a46c423d9589800d80a600ea2a
choihuk <love72huk@naver.com>
2023-06-02 21:31:08 +0900

    :sparkles: feature: findAllByUserUserIdAndDiaryDateBetween repository 메서드 테스트 구현

--- src/test/java/tipitapi/drawmytoday/common/testdata/TestEmotion.java
+++ src/test/java/tipitapi/drawmytoday/common/testdata/TestEmotion.java
@@ -1,5 +1,6 @@
 package tipitapi.drawmytoday.common.testdata;
 
+import org.springframework.test.util.ReflectionTestUtils;
 import tipitapi.drawmytoday.emotion.domain.Emotion;
 
 public class TestEmotion {
@@ -7,5 +8,11 @@ public class TestEmotion {
     public static Emotion createEmotion() {
         return Emotion.create("HAPPY", "#12312", true, "example emotion prompt",
             "example color prompt");
+    }
+
+    public static Emotion createEmotionWithId(Long emotionId) {
+        Emotion emotion = createEmotion();
+        ReflectionTestUtils.setField(emotion, "emotionId", emotionId);
+        return emotion;
     }
 }
