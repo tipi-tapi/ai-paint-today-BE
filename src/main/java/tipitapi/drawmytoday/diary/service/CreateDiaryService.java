f60181beadaf1ae48f0651583e58542438229458
akalswl14 <gnkryd12@gmail.com>
2023-06-03 18:58:12 +0900

    :sparkles: feature: 일기 생성 서비스 추가

--- src/main/java/tipitapi/drawmytoday/diary/service/CreateDiaryService.java
+++ src/main/java/tipitapi/drawmytoday/diary/service/CreateDiaryService.java
@@ -29,4 +29,45 @@ public class CreateDiaryService {
     private final S3Service s3Service;
     private final DallEService dallEService;
     private final PromptService promptService;
+
+    @Transactional(
+        noRollbackFor = {DallERequestFailException.class, DallERequestFailException.class,
+            ImageInputStreamFailException.class})
+    public CreateDiaryResponse createDiary(Long userId, Long emotionId, String keyword,
+        String notes) throws DallERequestFailException, ImageInputStreamFailException {
+        // TODO: 이미지 여러 개로 요청할 경우의 핸들링 필요
+        User user = validateUserService.validateUserById(userId);
+        Emotion emotion = validateEmotionService.validateEmotionById(emotionId);
+        String prompt = createPromptText(emotion, keyword);
+
+        try {
+            byte[] dallEImage = dallEService.getImageAsUrl(prompt);
+
+            Diary diary = diaryRepository.save(
+                Diary.builder().user(user).emotion(emotion).diaryDate(LocalDateTime.now())
+                    .notes(notes)
+                    .isAi(true).build());
+            promptService.createPrompt(diary, prompt, true);
+
+            String imagePath = getImagePath(diary.getDiaryId(), 1);
+            s3Service.uploadImage(dallEImage, imagePath);
+            imageService.createImage(diary, imagePath, true);
+            return new CreateDiaryResponse(diary.getDiaryId());
+        } catch (DallERequestFailException | ImageInputStreamFailException e) {
+            promptService.createPrompt(prompt, false);
+            throw e;
+        }
+    }
+
+    private String getImagePath(Long diaryId, int index) {
+        return String.format("post/%d/%s_%d.png", diaryId,
+            new Date().getTime(), index);
+    }
+
+    // TODO: 별도의 서비스로 분리, 로직 구현 필요
+    private String createPromptText(Emotion emotion, String keyword) {
+        return String.format(
+            "%s illustration in %s tones, painted in watercolor fairy tale style, with %s",
+            emotion.getEmotionPrompt(), emotion.getColorPrompt(), keyword);
+    }
 }
