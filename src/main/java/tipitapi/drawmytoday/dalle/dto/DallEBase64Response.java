1fceef23f486f31f6dcb3de5fbe9e4d0ea6d2c60
akalswl14 <gnkryd12@gmail.com>
2023-06-03 18:54:12 +0900

    :sparkles: feature: base64와 url로 dalle 응답받도록 세분화

---
+++ src/main/java/tipitapi/drawmytoday/dalle/dto/DallEBase64Response.java
@@ -0,0 +1,24 @@
+package tipitapi.drawmytoday.dalle.dto;
+
+import java.util.List;
+import lombok.Getter;
+import tipitapi.drawmytoday.dalle.exception.DallERequestFailException;
+
+@Getter
+public class DallEBase64Response {
+
+    private List<DallEBase64> data;
+
+    public String getBase64(int index) throws DallERequestFailException {
+        if (this.getData() == null || this.getData().isEmpty()) {
+            throw new DallERequestFailException();
+        }
+        return this.data.get(index).getB64_json();
+    }
+
+    @Getter
+    public static class DallEBase64 {
+
+        private String b64_json;
+    }
+}
