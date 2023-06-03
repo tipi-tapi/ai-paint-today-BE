45ac73c71cc220239021696eff814e12543bec38
akalswl14 <gnkryd12@gmail.com>
2023-06-03 18:58:05 +0900

    :sparkles: feature: DallE 요청 헤더, requestbody, 응답 방식 개선

--- src/main/java/tipitapi/drawmytoday/dalle/service/DallEService.java
+++ src/main/java/tipitapi/drawmytoday/dalle/service/DallEService.java
@@ -1,6 +1,9 @@
 package tipitapi.drawmytoday.dalle.service;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Base64;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -8,10 +11,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import tipitapi.drawmytoday.dalle.dto.CreateImageRequest;
-import tipitapi.drawmytoday.dalle.dto.CreateImageResponse;
+import tipitapi.drawmytoday.dalle.dto.DallEBase64Response;
+import tipitapi.drawmytoday.dalle.dto.DallEUrlResponse;
 import tipitapi.drawmytoday.dalle.exception.DallERequestFailException;
+import tipitapi.drawmytoday.dalle.exception.ImageInputStreamFailException;
 
 @Service
 @Transactional(readOnly = true)
@@ -19,29 +25,55 @@ public class DallEService {
 
     private final RestTemplate restTemplate;
     private final String apiUrl;
+    private final HttpHeaders requestHeader;
+
 
     public DallEService(@Qualifier("openaiRestTemplate") RestTemplate restTemplate,
         @Value("${openai.dalle.url}") String apiUrl) {
         this.restTemplate = restTemplate;
         this.apiUrl = apiUrl;
+        this.requestHeader = new HttpHeaders() {
+            {
+                setContentType(MediaType.APPLICATION_JSON);
+            }
+        };
     }
 
-    public byte[] getDallEImage(String prompt) {
-        HttpHeaders requestHeader = new HttpHeaders();
-        requestHeader.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<CreateImageRequest> request = new HttpEntity<>(CreateImageRequest.of(prompt),
-            requestHeader);
+    public byte[] getImageAsBase64(String prompt) throws DallERequestFailException {
+        try {
+            HttpEntity<CreateImageRequest> request = getRequest(
+                CreateImageRequest.withBase64(prompt));
 
-        CreateImageResponse response = restTemplate
-            .postForObject(apiUrl, request, CreateImageResponse.class);
-        validateResponse(response);
+            DallEBase64Response response = Optional.ofNullable(
+                restTemplate.postForObject(apiUrl, request, DallEBase64Response.class)
+            ).orElseThrow(DallERequestFailException::new);
 
-        return Base64.getDecoder().decode(response.getData().get(0).getB64_json());
-    }
-
-    private void validateResponse(CreateImageResponse response) {
-        if (response == null || response.getData() == null || response.getData().isEmpty()) {
-            throw new DallERequestFailException();
+            return Base64.getDecoder().decode(
+                response.getBase64(0)
+            );
+        } catch (HttpClientErrorException e) {
+            throw new DallERequestFailException(e);
         }
+    }
+
+    public byte[] getImageAsUrl(String prompt)
+        throws DallERequestFailException, ImageInputStreamFailException {
+        try {
+            HttpEntity<CreateImageRequest> request = getRequest(CreateImageRequest.withUrl(prompt));
+
+            String url = Optional.ofNullable(
+                    restTemplate.postForObject(apiUrl, request, DallEUrlResponse.class)
+                ).orElseThrow(DallERequestFailException::new)
+                .getUrl(0);
+            return new URL(url).openStream().readAllBytes();
+        } catch (HttpClientErrorException e) {
+            throw new DallERequestFailException(e);
+        } catch (IOException e) {
+            throw new ImageInputStreamFailException();
+        }
+    }
+
+    private HttpEntity<CreateImageRequest> getRequest(CreateImageRequest requestDto) {
+        return new HttpEntity<>(requestDto, requestHeader);
     }
 }
