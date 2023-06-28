a96b865609f5f974da697eb181ca6a4844aaf21a
choihuk <love72huk@naver.com>
2023-06-28 21:32:29 +0900

    :white_check_mark: test: findByUserId, findAllByEmail 메서드 테스트 구현

--- src/test/java/tipitapi/drawmytoday/diary/repository/UserRepositoryTest.java
+++ src/test/java/tipitapi/drawmytoday/diary/repository/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package tipitapi.drawmytoday.diary.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -10,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import tipitapi.drawmytoday.common.BaseRepositoryTest;
+import tipitapi.drawmytoday.common.testdata.TestUser;
+import tipitapi.drawmytoday.user.domain.SocialCode;
 import tipitapi.drawmytoday.user.domain.User;
 
 @DataJpaTest
@@ -18,7 +21,7 @@ public class UserRepositoryTest extends BaseRepositoryTest {
 
     @Nested
     @DisplayName("findByUserId 메소드 테스트")
-    class findByUserIdTest {
+    class FindByUserIdTest {
 
         @Nested
         @DisplayName("유저가 존재하는 경우")
@@ -33,6 +36,102 @@ public class UserRepositoryTest extends BaseRepositoryTest {
 
                 assertThat(findUser.isPresent()).isTrue();
                 assertThat(findUser.get().getUserId()).isEqualTo(user.getUserId());
+            }
+        }
+
+        @Nested
+        @DisplayName("삭제된 유저가 존재할 경우")
+        class If_deleted_user_exists {
+
+            @Test
+            @DisplayName("null을 반환한다.")
+            void return_null() {
+                User user = TestUser.createUser();
+                user.deleteUser();
+                userRepository.save(user);
+
+                Optional<User> findUser = userRepository.findByUserId(user.getUserId());
+
+                assertThat(findUser).isEmpty();
+            }
+        }
+
+        @Nested
+        @DisplayName("유저가 존재하지 않는 경우")
+        class If_user_not_exist {
+
+            @Test
+            @DisplayName("null을 반환한다.")
+            void return_null() {
+                Long notExistUserId = 1L;
+
+                Optional<User> findUser = userRepository.findByUserId(notExistUserId);
+
+                assertThat(findUser).isEmpty();
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("findAllByEmail 메서드 테스트")
+    class FindAllByEmailTest {
+
+        private final String email = "example@email.com";
+        private final String otherEmail = "aaaa@email.com";
+
+        @Nested
+        @DisplayName("이메일에 해당하는 유저가 2명일 경우")
+        class If_two_user_exists {
+
+            @Test
+            @DisplayName("두 유저를 반환한다.")
+            void return_two_user_list() {
+                User googleUser = User.createWithEmail(email, SocialCode.GOOGLE);
+                User appleUser = User.createWithEmail(email, SocialCode.APPLE);
+                User otherUser = User.createWithEmail(otherEmail, SocialCode.GOOGLE);
+                userRepository.save(googleUser);
+                userRepository.save(appleUser);
+                userRepository.save(otherUser);
+
+                List<User> findUserList = userRepository.findAllByEmail(email);
+
+                assertThat(findUserList).containsExactlyInAnyOrder(googleUser, appleUser);
+                assertThat(findUserList).hasSize(2);
+            }
+        }
+
+        @Nested
+        @DisplayName("이메일에 해당하는 유저가 한 명일 경우")
+        class If_one_user_exists {
+
+            @Test
+            @DisplayName("유저를 반환한다.")
+            void return_one_user_list() {
+                User googleUser = User.createWithEmail(email, SocialCode.GOOGLE);
+                User otherUser = User.createWithEmail(otherEmail, SocialCode.GOOGLE);
+                userRepository.save(googleUser);
+                userRepository.save(otherUser);
+
+                List<User> findUserList = userRepository.findAllByEmail(email);
+
+                assertThat(findUserList).containsExactly(googleUser);
+                assertThat(findUserList).hasSize(1);
+            }
+        }
+
+        @Nested
+        @DisplayName("이메일에 해당하는 유저가 없을 경우")
+        class If_user_not_exists {
+
+            @Test
+            @DisplayName("빈 리스트를 반환한다.")
+            void return_empty_list() {
+                User otherUser = User.createWithEmail(otherEmail, SocialCode.GOOGLE);
+                userRepository.save(otherUser);
+                
+                List<User> findUserList = userRepository.findAllByEmail(email);
+
+                assertThat(findUserList).isEmpty();
             }
         }
     }
