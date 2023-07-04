a81f30c959826460fd4d140e134c9354cd6ae6da
akalswl14 <gnkryd12@gmail.com>
2023-07-04 23:19:12 +0900

    :bug: bug: 테스트용 SQL 파일에서 AdType 삭제

--- src/test/resources/tipitapi/drawmytoday/adreward/repository/ExpiredAdReward.sql
+++ src/test/resources/tipitapi/drawmytoday/adreward/repository/ExpiredAdReward.sql
@@ -2,7 +2,7 @@ INSERT INTO `user` (user_id, created_at, updated_at, deleted_at, email, last_dia
                     social_code, user_role)
 VALUES (1, '2023-03-01T15:20:02.236278', '2023-03-01T15:20:02.236278', null, null, null, 'GOOGLE',
         null);
-INSERT INTO `ad_reward` (ad_reward_id, created_at, ad_type, used_at, user_id)
-VALUES (1, '2023-04-01T09:20:02.236278', 'VIDEO', null, 1);
-INSERT INTO `ad_reward` (ad_reward_id, created_at, ad_type, used_at, user_id)
-VALUES (2, '2023-06-30T08:20:02.236278', 'VIDEO', '2023-07-01T04:20:02.012345', 1);
+INSERT INTO `ad_reward` (ad_reward_id, created_at, used_at, user_id)
+VALUES (1, '2023-04-01T09:20:02.236278', null, 1);
+INSERT INTO `ad_reward` (ad_reward_id, created_at, used_at, user_id)
+VALUES (2, '2023-06-30T08:20:02.236278', '2023-07-01T04:20:02.012345', 1);
