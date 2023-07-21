d5a85f04664c877e11d997b0360a9b084bc86e2e
Hyeok_Choi <love72huk@naver.com>
2023-07-21 15:31:27 +0900

    :sparkles: feature: apple client secret 만료시 런타임에 바뀌도록 변경

--- src/main/java/tipitapi/drawmytoday/oauth/properties/AppleProperties.java
+++ src/main/java/tipitapi/drawmytoday/oauth/properties/AppleProperties.java
@@ -1,16 +1,19 @@
 package tipitapi.drawmytoday.oauth.properties;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
-@NoArgsConstructor
+@RequiredArgsConstructor
 @PropertySource("classpath:application-oauth.yml")
 public class AppleProperties {
+
+    private final AppleClientSecret clientSecret;
+    private final ApplePrivateKey privateKey;
 
     @Value("${oauth2.apple.ios.client-id}")
     private String clientId;
@@ -18,14 +21,12 @@ public class AppleProperties {
     private String teamId;
     @Value("${oauth2.apple.ios.key-id}")
     private String keyId;
-    @Value("${oauth2.apple.ios.private-key}")
-    private String privateKey;
     @Value("${oauth2.apple.ios.token-url}")
     private String tokenUrl;
     @Value("${oauth2.apple.ios.delete-account-url}")
     private String deleteAccountUrl;
 
     public String getClientSecret() {
-        return null;
+        return clientSecret.getClientSecret(teamId, keyId, privateKey.getPrivateKey(), clientId);
     }
 }
