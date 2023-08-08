4dd3a7d506d370df0137dd6b28fc735b8a889601
akalswl14 <gnkryd12@gmail.com>
2023-08-08 15:49:27 +0900

    :sparkles: feature: 관리자용 일기 목록 조회시, PresignedURL생성 호출은 일기가 책임져야함

--- src/main/java/tipitapi/drawmytoday/admin/service/AdminService.java
+++ src/main/java/tipitapi/drawmytoday/admin/service/AdminService.java
@@ -1,14 +1,12 @@
 package tipitapi.drawmytoday.admin.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tipitapi.drawmytoday.admin.dto.GetDiaryAdminResponse;
 import tipitapi.drawmytoday.diary.service.AdminDiaryService;
-import tipitapi.drawmytoday.s3.service.S3PreSignedService;
 import tipitapi.drawmytoday.user.service.ValidateUserService;
 
 @Service
@@ -18,20 +16,10 @@ public class AdminService {
 
     private final ValidateUserService validateUserService;
     private final AdminDiaryService adminDiaryService;
-    private final S3PreSignedService s3PreSignedService;
-    @Value("${presigned-image.expiration.admin-diaries}")
-    private int getDiariesExpiration;
 
     public Page<GetDiaryAdminResponse> getDiaries(Long userId, int size, int page,
         Direction direction) {
         validateUserService.validateAdminUserById(userId);
-        return adminDiaryService.getDiaries(size, page, direction).map(this::buildImageUrl);
-    }
-
-    private GetDiaryAdminResponse buildImageUrl(GetDiaryAdminResponse response) {
-        String imageUrl = s3PreSignedService.getPreSignedUrlForShare(response.getImageURL(),
-            getDiariesExpiration);
-        response.updateImageUrl(imageUrl);
-        return response;
+        return adminDiaryService.getDiaries(size, page, direction);
     }
 }
--- src/main/java/tipitapi/drawmytoday/diary/service/AdminDiaryService.java
+++ src/main/java/tipitapi/drawmytoday/diary/service/AdminDiaryService.java
@@ -1,6 +1,7 @@
 package tipitapi.drawmytoday.diary.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tipitapi.drawmytoday.admin.dto.GetDiaryAdminResponse;
 import tipitapi.drawmytoday.diary.repository.DiaryRepository;
+import tipitapi.drawmytoday.s3.service.S3PreSignedService;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,9 +17,18 @@ import tipitapi.drawmytoday.diary.repository.DiaryRepository;
 public class AdminDiaryService {
 
     private final DiaryRepository diaryRepository;
+    private final S3PreSignedService s3PreSignedService;
+    @Value("${presigned-image.expiration.admin-diaries}")
+    private int imageExpiration;
 
     public Page<GetDiaryAdminResponse> getDiaries(int size, int page, Direction direction) {
         return diaryRepository.getDiariesForMonitorAsPage(
-            Pageable.ofSize(size).withPage(page), direction);
+            Pageable.ofSize(size).withPage(page), direction).map(this::generatePresignedURL);
+    }
+
+    private GetDiaryAdminResponse generatePresignedURL(GetDiaryAdminResponse response) {
+        response.updateImageUrl(
+            s3PreSignedService.getPreSignedUrlForShare(response.getImageURL(), imageExpiration));
+        return response;
     }
 }
