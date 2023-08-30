ee449b7f947e3259c81e0a42ca0a3dbcf0ea0a15
Hyeok_Choi <love72huk@naver.com>
2023-08-30 14:16:33 +0900

    :sparkles: feature: 스프링 캐시 도입

--- src/main/java/tipitapi/drawmytoday/DrawMyTodayApplication.java
+++ src/main/java/tipitapi/drawmytoday/DrawMyTodayApplication.java
@@ -2,8 +2,10 @@ package tipitapi.drawmytoday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class DrawMyTodayApplication {
