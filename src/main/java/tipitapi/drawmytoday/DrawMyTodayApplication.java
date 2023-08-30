6541a50f54cc3625a32c702d7f69f38bdc0de811
Hyeok_Choi <love72huk@naver.com>
2023-08-30 14:48:50 +0900

    :hammer: refactor: @EnableCache 삭제

--- src/main/java/tipitapi/drawmytoday/DrawMyTodayApplication.java
+++ src/main/java/tipitapi/drawmytoday/DrawMyTodayApplication.java
@@ -2,10 +2,8 @@ package tipitapi.drawmytoday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class DrawMyTodayApplication {
