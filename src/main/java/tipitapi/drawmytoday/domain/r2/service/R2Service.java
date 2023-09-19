0e645648bd25760ec7e77fafd1c6cb780f456d96
akalswl14 <gnkryd12@gmail.com>
2023-09-19 17:23:51 +0900

    :sparkles: feature: 이미지 업로드 mocking

--- src/main/java/tipitapi/drawmytoday/domain/r2/service/R2Service.java
+++ src/main/java/tipitapi/drawmytoday/domain/r2/service/R2Service.java
@@ -5,12 +5,8 @@ import static software.amazon.awssdk.services.s3.model.ObjectCannedACL.PRIVATE;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.core.exception.SdkClientException;
-import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
-import tipitapi.drawmytoday.domain.r2.exception.R2FailedException;
 
 @Service
 @RequiredArgsConstructor
@@ -21,14 +17,15 @@ public class R2Service {
     private String bucketName;
 
     public void uploadImage(byte[] imageBytes, String filePath) {
-        try {
-            PutObjectRequest putObjectRequest = buildPutObjectRequest(filePath);
-            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(imageBytes));
-        } catch (SdkClientException | S3Exception e) {
-            throw e;
-        } catch (Exception e) {
-            throw new R2FailedException(e);
-        }
+        // mocking 이므로 주석 처리
+//        try {
+//            PutObjectRequest putObjectRequest = buildPutObjectRequest(filePath);
+//            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(imageBytes));
+//        } catch (SdkClientException | S3Exception e) {
+//            throw e;
+//        } catch (Exception e) {
+//            throw new R2FailedException(e);
+//        }
     }
 
     private PutObjectRequest buildPutObjectRequest(String filePath) {
