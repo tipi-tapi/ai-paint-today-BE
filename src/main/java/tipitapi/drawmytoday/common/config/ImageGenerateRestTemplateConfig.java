5d27bbffc8881da6641569226a7e2791177c64b3
Hyeok_Choi <love72huk@naver.com>
2023-09-25 14:15:05 +0900

    :hammer: refactor: OpenApiRestTemplateConfig -> ImageGenerateRestTemplateConfig 이름 변경

--- src/main/java/tipitapi/drawmytoday/common/config/ImageGenerateRestTemplateConfig.java
+++ src/main/java/tipitapi/drawmytoday/common/config/ImageGenerateRestTemplateConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class OpenAIRestTemplateConfig {
+public class ImageGenerateRestTemplateConfig {
 
     @Value("${openai.api.key}")
     private String openaiApiKey;
