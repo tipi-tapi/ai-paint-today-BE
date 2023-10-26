10580f781c26a2ded1c45b94611781128c226ef1
Hyeok_Choi <love72huk@naver.com>
2023-10-26 17:50:50 +0900

    :white_check_mark: test: karloServiceTest generateTestImage 메서드 검증 로직 추가

--- src/test/java/tipitapi/drawmytoday/domain/generator/domain/karlo/service/KarloServiceTest.java
+++ src/test/java/tipitapi/drawmytoday/domain/generator/domain/karlo/service/KarloServiceTest.java
@@ -8,6 +8,9 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -16,6 +19,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import tipitapi.drawmytoday.common.testdata.TestPrompt;
+import tipitapi.drawmytoday.domain.diary.dto.CreateTestDiaryRequest;
+import tipitapi.drawmytoday.domain.diary.dto.CreateTestDiaryRequest.KarloParameter;
 import tipitapi.drawmytoday.domain.diary.service.PromptService;
 import tipitapi.drawmytoday.domain.generator.domain.karlo.exception.KarloRequestFailException;
 import tipitapi.drawmytoday.domain.generator.dto.GeneratedImageAndPrompt;
@@ -30,12 +35,12 @@ class KarloServiceTest {
     @InjectMocks
     KarloService karloService;
 
-    @DisplayName("generateImage(Prompt) 메서드는")
     @Nested
-    class GenerateImageTest {
+    @DisplayName("generateImage(Prompt) 메서드 테스트")
+    class GenerateImage_test {
 
-        @DisplayName("karlo 요청을 실패할 경우 fail prompt를 저장한다.")
         @Test
+        @DisplayName("karlo 요청을 실패할 경우 fail prompt를 저장한다.")
         void karlo_request_fail_then_generateImage_fail() throws Exception {
             // given
             String prompt = "prompt";
@@ -50,8 +55,8 @@ class KarloServiceTest {
             verify(promptService).createPrompt(any(String.class), eq(false));
         }
 
-        @DisplayName("karlo 요청을 성공할 경우 성공한 prompt를 저장한다.")
         @Test
+        @DisplayName("karlo 요청을 성공할 경우 성공한 prompt를 저장한다.")
         void karlo_request_success_then_generateImage_success() throws Exception {
             // given
             String prompt = "prompt";
@@ -69,4 +74,46 @@ class KarloServiceTest {
         }
     }
 
+    @Nested
+    @DisplayName("generateTestImage 메서드 테스트")
+    class GenerateTestImage_test {
+
+        @Test
+        @DisplayName("karlo 요청을 실패할 경우 fail prompt를 저장한다.")
+        void karlo_request_fail_then_generateImage_fail() throws Exception {
+            // given
+            KarloParameter karloParameter = new KarloParameter("prompt", "negativePrompt",
+                1, 10, 10D, null);
+            CreateTestDiaryRequest request = new CreateTestDiaryRequest(1L, "notes",
+                LocalDate.now(), LocalTime.now(), karloParameter);
+            given(karloRequestService.getTestImageAsUrl(any(KarloParameter.class)))
+                .willThrow(KarloRequestFailException.class);
+
+            // when
+            // then
+            assertThatThrownBy(() -> karloService.generateTestImage(request))
+                .isInstanceOf(KarloRequestFailException.class);
+            verify(promptService).createPrompt(any(String.class), eq(false));
+        }
+
+        @Test
+        @DisplayName("karlo 요청을 성공할 경우 이미지들을 반환한다.")
+        void karlo_request_success_then_return_images() throws Exception {
+            // given
+            KarloParameter karloParameter = new KarloParameter("prompt", "negativePrompt",
+                1, 10, 10D, null);
+            CreateTestDiaryRequest request = new CreateTestDiaryRequest(1L, "notes",
+                LocalDate.now(), LocalTime.now(), karloParameter);
+            List<byte[]> images = List.of(new byte[0]);
+            given(karloRequestService.getTestImageAsUrl(any(KarloParameter.class)))
+                .willReturn(images);
+
+            // when
+            List<byte[]> returnImages = karloService.generateTestImage(request);
+
+            // then
+            verify(promptService, never()).createPrompt(any(String.class), eq(false));
+            assertThat(returnImages).isEqualTo(images);
+        }
+    }
 }
