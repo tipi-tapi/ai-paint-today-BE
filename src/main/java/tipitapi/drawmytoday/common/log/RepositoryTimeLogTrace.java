1c1e100157d31796f2103b07e28ae0c6029f6929
Hyeok_Choi <love72huk@naver.com>
2023-12-30 19:10:53 +0900

    :sparkles: feature: Repository단 Log Trace 로직 생성

---
+++ src/main/java/tipitapi/drawmytoday/common/log/RepositoryTimeLogTrace.java
@@ -0,0 +1,25 @@
+package tipitapi.drawmytoday.common.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class RepositoryTimeLogTrace {
+
+    @Around("execution(* tipitapi.drawmytoday.domain.*.repository.*Repository.*(..)) "
+        + "&& !execution(* tipitapi.drawmytoday.domain.*.repository.*Impl.*(..))")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long finish = System.currentTimeMillis();
+        String[] typeName = joinPoint.getSignature().getDeclaringTypeName().split("\\.");
+        log.info("[{}] [{}] [{}]", typeName[typeName.length - 1],
+            joinPoint.getSignature().getName(), finish - start + "ms");
+        return proceed;
+    }
+}
