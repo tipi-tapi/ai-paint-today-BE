334645d0877d510a4f3f03cbf356d3af85d1fb85
Hyeok_Choi <love72huk@naver.com>
2024-02-16 18:23:32 +0900

    :hammer: refactor: entity 구조 변경

--- src/main/java/tipitapi/drawmytoday/domain/diary/domain/Prompt.java
+++ src/main/java/tipitapi/drawmytoday/domain/diary/domain/Prompt.java
@@ -2,12 +2,9 @@ package tipitapi.drawmytoday.domain.diary.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -23,10 +20,6 @@ public class Prompt extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long promptId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "diary_id", nullable = true)
-    private Diary diary;
-
     @NotNull
     @Column(length = 1100)
     private String promptText;
@@ -34,19 +27,9 @@ public class Prompt extends BaseEntity {
     @NotNull
     private boolean isSuccess;
 
-    private Prompt(Diary diary, String promptText, boolean isSuccess) {
-        this.diary = diary;
-        this.promptText = promptText;
-        this.isSuccess = isSuccess;
-    }
-
     private Prompt(String promptText, boolean isSuccess) {
         this.promptText = promptText;
         this.isSuccess = isSuccess;
-    }
-
-    public static Prompt create(Diary diary, String promptText, boolean isSuccess) {
-        return new Prompt(diary, promptText, isSuccess);
     }
 
     public static Prompt create(String promptText, boolean isSuccess) {
