c2ddd4696de739ee79c45c3a71d6021851758d68
Hyeok_Choi <love72huk@naver.com>
2024-02-28 20:23:21 +0900

    :hammer: refactor: openai, karlo RestTemplate timeout 설정

--- src/main/java/tipitapi/drawmytoday/common/config/ImageGenerateRestTemplateConfig.java
+++ src/main/java/tipitapi/drawmytoday/common/config/ImageGenerateRestTemplateConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -14,11 +15,14 @@ public class ImageGenerateRestTemplateConfig {
 
     @Value("${kakao.api.key}")
     private String kakaoApiKey;
+    private static final int DEFAULT_CONNECTION_TIMEOUT = 1000 * 30;
 
     @Bean
     @Qualifier("openaiRestTemplate")
     public RestTemplate openaiRestTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(DEFAULT_CONNECTION_TIMEOUT);
+        RestTemplate restTemplate = new RestTemplate(factory);
         restTemplate.getInterceptors().add((request, body, execution) -> {
             request.getHeaders().add("Authorization", "Bearer " + openaiApiKey);
             return execution.execute(request, body);
@@ -28,7 +32,9 @@ public class ImageGenerateRestTemplateConfig {
 
     @Bean
     public RestTemplate karloRestTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(DEFAULT_CONNECTION_TIMEOUT);
+        RestTemplate restTemplate = new RestTemplate(factory);
         restTemplate.getInterceptors().add((request, body, execution) -> {
             request.getHeaders().add("Authorization", "KakaoAK " + kakaoApiKey);
             return execution.execute(request, body);
