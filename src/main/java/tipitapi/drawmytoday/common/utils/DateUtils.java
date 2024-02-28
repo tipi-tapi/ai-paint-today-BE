9affb589d5417bcec5aa37a2671abe2f46d9ad2e
Hyeok_Choi <love72huk@naver.com>
2024-02-29 00:41:12 +0900

    :hammer: refactor: DiaryUtil 2월 29일도 통과되도록 핫픽스

--- src/main/java/tipitapi/drawmytoday/common/utils/DateUtils.java
+++ src/main/java/tipitapi/drawmytoday/common/utils/DateUtils.java
@@ -41,7 +41,7 @@ public class DateUtils {
 
     private static int getMaxDay(int month) {
         if (month == 2) {
-            return 28;
+            return 29;
         }
         if (Stream.of(1, 3, 5, 7, 8, 10, 12)
             .anyMatch(m -> m == month)) {
