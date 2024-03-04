952c5ebf1a7ac2b309181aa0f86f8c203350250e
Hyeok_Choi <love72huk@naver.com>
2024-03-04 17:01:04 +0900

    :hammer: refactor: DallEService 에러 해결

--- src/main/java/tipitapi/drawmytoday/domain/generator/domain/dalle/service/DallEService.java
+++ src/main/java/tipitapi/drawmytoday/domain/generator/domain/dalle/service/DallEService.java
@@ -8,10 +8,6 @@ import tipitapi.drawmytoday.domain.diary.domain.Prompt;
 import tipitapi.drawmytoday.domain.diary.dto.CreateTestDiaryRequest;
 import tipitapi.drawmytoday.domain.diary.service.PromptService;
 import tipitapi.drawmytoday.domain.diary.service.PromptTextService;
-import tipitapi.drawmytoday.domain.emotion.domain.Emotion;
-import tipitapi.drawmytoday.domain.generator.domain.dalle.exception.DallEPolicyViolationException;
-import tipitapi.drawmytoday.domain.generator.domain.dalle.exception.DallERequestFailException;
-import tipitapi.drawmytoday.domain.generator.dto.GeneratedImageAndPrompt;
 import tipitapi.drawmytoday.domain.generator.exception.ImageGeneratorException;
 import tipitapi.drawmytoday.domain.generator.service.ImageGeneratorService;
 
@@ -24,51 +20,51 @@ public class DallEService implements ImageGeneratorService {
     private final PromptService promptService;
     private final DallERequestService dalleRequestService;
 
-    @Transactional(noRollbackFor = ImageGeneratorException.class)
-    public GeneratedImageAndPrompt generateImage(Emotion emotion, String keyword)
-        throws ImageGeneratorException {
-        String prompt = promptTextService.createPromptText(emotion, keyword);
-        try {
-            byte[] image = dalleRequestService.getImageAsUrl(prompt);
-            return new GeneratedImageAndPrompt(prompt, image);
-        } catch (DallEPolicyViolationException e) {
-            promptService.createPrompt(prompt, false);
-            return reGenerateImage(emotion);
-        } catch (ImageGeneratorException e) {
-            promptService.createPrompt(prompt, false);
-            throw e;
-        }
-    }
-
-    private GeneratedImageAndPrompt reGenerateImage(Emotion emotion)
-        throws ImageGeneratorException {
-        String prompt = promptTextService.createPromptText(emotion, null);
-        try {
-            byte[] image = dalleRequestService.getImageAsUrl(prompt);
-            return new GeneratedImageAndPrompt(prompt, image);
-        } catch (ImageGeneratorException e) {
-            promptService.createPrompt(prompt, false);
-            throw (e instanceof DallEPolicyViolationException) ?
-                DallERequestFailException.violatePolicy() : e;
-        }
-    }
-
-
-    @Transactional(noRollbackFor = ImageGeneratorException.class)
-    public GeneratedImageAndPrompt generateImage(Prompt prompt) throws ImageGeneratorException {
-        try {
-            byte[] image = dalleRequestService.getImageAsUrl(prompt.getPromptText());
-            return new GeneratedImageAndPrompt(prompt.getPromptText(), image);
-        } catch (ImageGeneratorException e) {
-            promptService.createPrompt(prompt.getPromptText(), false);
-            throw (e instanceof DallEPolicyViolationException) ?
-                DallERequestFailException.violatePolicy() : e;
-        }
-    }
+//    @Transactional(noRollbackFor = ImageGeneratorException.class)
+//    public GeneratedImageAndPrompt generateImage(Emotion emotion, String keyword)
+//        throws ImageGeneratorException {
+//        String prompt = promptTextService.createPrompt(emotion, keyword);
+//        try {
+//            byte[] image = dalleRequestService.getImageAsUrl(prompt);
+//            return new GeneratedImageAndPrompt(prompt, image);
+//        } catch (DallEPolicyViolationException e) {
+//            promptService.createPrompt(prompt, false);
+//            return reGenerateImage(emotion);
+//        } catch (ImageGeneratorException e) {
+//            promptService.createPrompt(prompt, false);
+//            throw e;
+//        }
+//    }
+//
+//    private GeneratedImageAndPrompt reGenerateImage(Emotion emotion)
+//        throws ImageGeneratorException {
+//        String prompt = promptTextService.createPrompt(emotion, null);
+//        try {
+//            byte[] image = dalleRequestService.getImageAsUrl(prompt);
+//            return new GeneratedImageAndPrompt(prompt, image);
+//        } catch (ImageGeneratorException e) {
+//            promptService.createPrompt(prompt, false);
+//            throw (e instanceof DallEPolicyViolationException) ?
+//                DallERequestFailException.violatePolicy() : e;
+//        }
+//    }
+//
+//
+//    @Transactional(noRollbackFor = ImageGeneratorException.class)
+//    public GeneratedImageAndPrompt generateImage(Prompt prompt) throws ImageGeneratorException {
+//        try {
+//            byte[] image = dalleRequestService.getImageAsUrl(prompt.getPromptText());
+//            return new GeneratedImageAndPrompt(prompt.getPromptText(), image);
+//        } catch (ImageGeneratorException e) {
+//            promptService.createPrompt(prompt.getPromptText(), false);
+//            throw (e instanceof DallEPolicyViolationException) ?
+//                DallERequestFailException.violatePolicy() : e;
+//        }
+//    }
 
     @Override
-    public byte[] generateImage(String promptText) throws ImageGeneratorException {
-        return new byte[0];
+    public byte[] generateImage(Prompt prompt) throws ImageGeneratorException {
+        throw new UnsupportedOperationException();
     }
 
     @Override
